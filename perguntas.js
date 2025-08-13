780102741d2e2c7a5395f3843d1ae3b94b5a1785
Karina <karina.souza21@escola.pr.gov.br>
2025-08-13 19:48:58 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Felicidade',
+    'O que precisa para ser feliz?',
+    ' Fazer o que gosta!!!'
+)
+
+criaCartao(
+    'Informática',
+    'Qual o melhor canal para aprender informática',
+    'RasinformaticA'
+)
+
+criaCartao(
+    'Qual é a melhor turma do CERB',
+    'A que mais se dedica nas aulas do Prof. Rafael?',
+    'Estou em dúvida'
+)
+
+criaCartao(
+    'Código',
+    'O que è ?',
+    '"O código é poesia lógica que transforma ideias em realidade."'
+)
